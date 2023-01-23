e21347a534b32bc2cd542ad6d0cf7ceb90980ceb
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-01-23 09:28:55 +0300

    Mongoose Population

--- routes/dishRouter.js
+++ routes/dishRouter.js
@@ -12,6 +12,7 @@ dishRouter.use(bodyParser.json());
 dishRouter.route('/')
     .get((req, res, next) => {
         Dishes.find({})
+        .populate('comments.author')
         .then((dishes) => {
             res.statusCode = 200    
             res.setHeader('Content-Types', 'application/json');
@@ -46,6 +47,7 @@ dishRouter.route('/')
 dishRouter.route('/:dishId')
     .get((req, res, next) => {
         Dishes.findById(req.params.dishId)
+        .populate('comments.author')
         .then((dish) => {
             res.statusCode = 200    
             res.setHeader('Content-Types', 'application/json');
@@ -81,6 +83,7 @@ dishRouter.route('/:dishId')
 dishRouter.route('/:dishId/comments')
     .get((req, res, next) => {
         Dishes.findById(req.params.dishId)
+        .populate('comments.author')
         .then((dish) => {
             if (dish != null) {
                 res.statusCode = 200    
@@ -98,11 +101,19 @@ dishRouter.route('/:dishId/comments')
         Dishes.findById(req.params.dishId)
         .then((dish) => {
             if (dish != null) {
+                // the user data is obtained through 
+                // Passport/JWT authenticate.verifyUser 
+                req.body.author = req.user._id
                 dish.comments.push(req.body);
-                dish.save().then((dish)  => {
-                    res.statusCode = 200    
-                    res.setHeader('Content-Types', 'application/json');
-                    res.json(dish);
+                dish.save()
+                .then((dish)  => {
+                    Dishes.findById(dish._id)
+                    .populate('comments.author')
+                    .then((dish) => {
+                        res.statusCode = 200    
+                        res.setHeader('Content-Types', 'application/json');
+                        res.json(dish);
+                    })
                 }, (err) => next(err));
             } else {
                 err = new Error('Dish: ' + req.params.dishId + ' not found');
@@ -141,6 +152,7 @@ dishRouter.route('/:dishId/comments')
 dishRouter.route('/:dishId/comments/:commentId')
     .get((req, res, next) => {
         Dishes.findById(req.params.dishId)
+        .populate('comments.author')
         .then((dish) => {
             if (dish != null && dish.comments.id(req.params.commentId) != null) {
                 res.statusCode = 200    
@@ -174,9 +186,13 @@ dishRouter.route('/:dishId/comments/:commentId')
                 } 
                 dish.save()
                 .then(() => {
-                    res.statusCode = 200;
-                    res.setHeader('Content-Types', 'application/json');
-                    res.json(dish);
+                    Dishes.findById(dish._id)
+                    .populate('comments.author')
+                    .then((dish) => {
+                        res.statusCode = 200;
+                        res.setHeader('Content-Types', 'application/json');
+                        res.json(dish);
+                    })
                 }, (err) => next(err));
             } else if (dish == null) {
                 err = new Error('Dish: ' + req.params.dishId + ' not found');
@@ -196,9 +212,13 @@ dishRouter.route('/:dishId/comments/:commentId')
                 dish.comments.id(req.params.commentId).remove();
                 dish.save()
                 .then((dish)  => {
-                    res.statusCode = 200    
-                    res.setHeader('Content-Types', 'application/json');
-                    res.json(dish);
+                    Dishes.findById(dish._id)
+                    .populate('comments.author')
+                    .then((dish) => {
+                        res.statusCode = 200;
+                        res.setHeader('Content-Types', 'application/json');
+                        res.json(dish);
+                    })
                 }, (err) => next(err));
             } else if (dish == null) {
                 err = new Error('Dish: ' + req.params.dishId + ' not found');
