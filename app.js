6c63520abdf26b47a1e7511a6239c9e75f5f354b
Yahya Saddiq <yahya.saddiq@gmail.com>
2023-01-23 15:27:42 +0300

    File Upload

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var usersRouter = require('./routes/usersRouter');
 var dishRouter = require('./routes/dishRouter');
 var promoRouter = require('./routes/promoRouter');
 var leaderRouter = require('./routes/leaderRouter');
+var uploadRouter = require('./routes/uploadRouter');
 
 const mongoose = require('mongoose');
 
@@ -70,6 +71,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
+app.use('/imageUpload', uploadRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
